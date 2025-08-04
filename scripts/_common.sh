0942cd6f1a2172bec042b1d1a43a767dc909edb4
Thomas <51749973+Thovi98@users.noreply.github.com>
2025-08-04 09:18:25 +0200

    fix vue folder (-> vue3)

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -18,7 +18,7 @@ _tandoor_venv_install() {
 }
 
 _tandoor_build_frontend() {
-    pushd "$install_dir/vue"
+    pushd "$install_dir/vue3"
         ynh_hide_warnings ynh_exec_as_app yarn install
         ynh_hide_warnings ynh_exec_as_app yarn build
     popd
